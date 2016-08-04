81dda50b238ce004ffc72ee56e0e1f01f574dfe7
Kamron Batman <kamronbatman@users.noreply.github.com>
2016-08-04 12:30:59 -0700

    [Update/Fix] Updates & Fixes tests (#1)
    * [Update] Updates all dependencies, code climate, and travis ci.
    
    * [Fix] Fixes node version.
    
    * [Fix] Fixes package.json
    
    * [Fix] Fixes standard requirements.
    
    * [Fix] Fixes tests.
    
    * [Revert] Reverts the API token for code climate.

--- test/knexfile.js
+++ test/knexfile.js
@@ -1,13 +1,14 @@
+var path = require('path')
 // Update with your config settings.
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: __dirname + '/dev.sqlite3'
+      filename: path.resolve(__dirname, 'dev.sqlite3')
     },
     migrations: {
-      directory: __dirname + '/migrations'
+      directory: path.resolve(__dirname, 'migrations')
     }
   }
 }
