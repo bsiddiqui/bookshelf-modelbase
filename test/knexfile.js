dda1c618d526a8b8a84861d5663c0954e213f1f4
André Santos <andre@dexdev.com.br>
2019-04-07 21:03:14 -0300

    Configurre SQLite in memory instead of file

--- test/knexfile.js
+++ test/knexfile.js
@@ -4,9 +4,11 @@ var path = require('path')
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: {
-      filename: path.resolve(__dirname, 'dev.sqlite3')
+    connection: ':memory:',
+    pool: {
+      size: 1
     },
+    useNullAsDefault: true,
     migrations: {
       directory: path.resolve(__dirname, 'migrations')
     }
